25ee05f174e2a7465468abd43007180d9974594a
Mauricio Degregori <degregori2@gmail.com>
2020-05-08 11:39:51 -0500

    ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
+#      starting location is the key and
+#   the destination is the value
+    route = []
+    dicty = {}
 
+    for i in tickets:
+        dicty[i.source] = i.destination
+
+    route.append(dicty["NONE"])
+    final_destination = dicty["NONE"]
+
+    while final_destination != "NONE":
+        if final_destination == "NONE":
+            continue
+        else:
+            route.append(dicty[final_destination])
+            final_destination = dicty[final_destination]
     return route
+  
+
+
