7f1197dedb60b31434c67d2c23f8f8a74366e836
Mauricio Degregori <degregori2@gmail.com>
2020-05-08 11:09:20 -0500

    ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,17 @@
 def has_negatives(a):
+    dicti = {}
+    result = []
 
-    """
-    YOUR CODE HERE
-    """
-
+    if len(a) < 1:
+        return None
+    # if index is greater than zero (ensures positive), then add them to dict 
+    for i in a:
+        if i > 0:
+            dicti[i] = i
+            
+    for i in a:
+        if i * -1 in dicti:
+            result.append(abs(i))
     return result
 
 
